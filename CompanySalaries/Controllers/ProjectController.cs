badb9983a3a5e0f7ea9076680520f32600ffc62f
Miruna Oltean <miruna123oltean@gmail.com>
2022-06-30 15:47:57 +0300

    Added repositories.

---
+++ CompanySalaries/Controllers/ProjectController.cs
@@ -0,0 +1,33 @@
+﻿using CompanySalaries.Models;
+using CompanySalaries.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanySalaries.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        public IProjectRepository projectRepository;
+
+        public ProjectController(IProjectRepository projectRepository)
+        {
+            this.projectRepository = projectRepository;
+        }
+
+        [HttpGet]
+        [Route("/GetAllProjects")]
+        public IEnumerable<Project> GetAllProjects()
+        {
+            return projectRepository.GetAllProjects();
+        }
+
+        [HttpPost]
+        [Route("/AddProject")]
+        public void AddProject(Project project)
+        {
+            projectRepository.AddProject(project);
+        }
+
+    }
+}
---
+++ CompanySalaries/Repositories/IProjectRepository.cs
@@ -0,0 +1,10 @@
+﻿using CompanySalaries.Models;
+
+namespace CompanySalaries.Repositories
+{
+    public interface IProjectRepository
+    {
+        public void AddProject(Project project);
+        public IEnumerable<Project> GetAllProjects();
+    }
+}
